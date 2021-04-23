8973c2fca8ae4ac2a66744a14fd90dd9be8aeb3a
Ellis Yoo <yjoo.eyoo@gmail.com>
2021-04-22 20:38:57 -0700

    comment out incomplete stretch goal

--- src/routes/v1.js
+++ src/routes/v1.js
@@ -5,48 +5,48 @@ const express = require('express')
 
 var wrapper = (req,res)=>{
 
-    const GenericCollection = require('../models/data-collection-class')
-    const path = req.params.path
-    const model = require(`../models/${path}`)
-    const router = express.Router()
+    // const GenericCollection = require('../models/data-collection-class')
+    // const path = req.params.path
+    // const model = require(`../models/${path}`)
+    // const router = express.Router()
     
-    const someName =  new GenericCollection(model)
+    // const someName =  new GenericCollection(model)
     
-    router.get(getAll)
-    router.get(`/:id`,getOne)
-    router.post(createItem)
-    router.put(`/:id`,updateItem)
-    router.delete(`/:id`,deleteItem)
+    // router.get(getAll)
+    // router.get(`/:id`,getOne)
+    // router.post(createItem)
+    // router.put(`/:id`,updateItem)
+    // router.delete(`/:id`,deleteItem)
     
-    async function getAll(request,response){
-        let items = await someName.read()
-        response.status(200).json(items)
-    }
+    // async function getAll(request,response){
+    //     let items = await someName.read()
+    //     response.status(200).json(items)
+    // }
     
-    async function getOne(request,response){
-        let id = request.params.id
-        let item = await someName.read(id)
-        response.status(200).json(item)
-    }
+    // async function getOne(request,response){
+    //     let id = request.params.id
+    //     let item = await someName.read(id)
+    //     response.status(200).json(item)
+    // }
     
-    async function createItem(request,response){
-        let data = rerequestq.body
-        let createItem = await someName.create(data)
-        response.status(201).json(createItem)
-    }
+    // async function createItem(request,response){
+    //     let data = rerequestq.body
+    //     let createItem = await someName.create(data)
+    //     response.status(201).json(createItem)
+    // }
     
-    async function updateItem(request,response){
-        let id = request.params.id
-        let data =request.body
-        let updateItem = await someName.update(id,data)
-        response.status(200).json(updateItem)
-    }
+    // async function updateItem(request,response){
+    //     let id = request.params.id
+    //     let data =request.body
+    //     let updateItem = await someName.update(id,data)
+    //     response.status(200).json(updateItem)
+    // }
     
-    async function deleteItem(request,response){
-        let id = request.params.id
-        await someName.delete(id)
-        response.status(200).json({msg:'deleted!'})
-    }
+    // async function deleteItem(request,response){
+    //     let id = request.params.id
+    //     await someName.delete(id)
+    //     response.status(200).json({msg:'deleted!'})
+    // }
 }
 
 
